0ff45d28977c93a34932e0321500d34d5657ebaa
ApilRijal2020 <70686944+ApilRijal2020@users.noreply.github.com>
2020-10-03 18:11:20 +1000

    Updates

---
+++ precache-manifest.9aba128e80431cabb0f2c8673ce5040e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce69bd3b03edf5d69b0e101e5625140",
+    "url": "/clicknsw/index.html"
+  },
+  {
+    "revision": "ffa2d8ab8e8c535da6ea",
+    "url": "/clicknsw/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "4424072b3e3c49975be5",
+    "url": "/clicknsw/static/css/main.3c7a00a4.chunk.css"
+  },
+  {
+    "revision": "ffa2d8ab8e8c535da6ea",
+    "url": "/clicknsw/static/js/2.0ef877d6.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/clicknsw/static/js/2.0ef877d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4424072b3e3c49975be5",
+    "url": "/clicknsw/static/js/main.50ca755e.chunk.js"
+  },
+  {
+    "revision": "a79910e4cf5e625f3296",
+    "url": "/clicknsw/static/js/runtime-main.379787c5.js"
+  },
+  {
+    "revision": "5ef4a9c8f9a0b6d7f22e30038e3eef1f",
+    "url": "/clicknsw/static/media/about.5ef4a9c8.png"
+  },
+  {
+    "revision": "6476174c0ab3bd8cb67a5c20a27a8730",
+    "url": "/clicknsw/static/media/backg.6476174c.jpg"
+  },
+  {
+    "revision": "5e63295cf032eebaa6a25888df701338",
+    "url": "/clicknsw/static/media/backgr.5e63295c.png"
+  },
+  {
+    "revision": "d683194712a3d5ca3f8c7fb5b9dbfd62",
+    "url": "/clicknsw/static/media/blend.d6831947.jpg"
+  },
+  {
+    "revision": "577f2a2b544d52b9d2c4ffc479e65906",
+    "url": "/clicknsw/static/media/coff.577f2a2b.png"
+  },
+  {
+    "revision": "f72f1fee19bce3f6a91daa9010ed5848",
+    "url": "/clicknsw/static/media/homee.f72f1fee.png"
+  },
+  {
+    "revision": "d598dddbcf01c995d24f77b73f647a42",
+    "url": "/clicknsw/static/media/one.d598dddb.jpg"
+  },
+  {
+    "revision": "02691026e34f8b382ca77e3ebbc71572",
+    "url": "/clicknsw/static/media/transport.02691026.png"
+  },
+  {
+    "revision": "d5687eff29ee127839788da09c224874",
+    "url": "/clicknsw/static/media/yel.d5687eff.png"
+  }
+]);
